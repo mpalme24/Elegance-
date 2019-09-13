d081f8575abe6c7c5119c486f4f0d5f802f8995d
mpalme24 <41879256+mpalme24@users.noreply.github.com>
2019-09-14 00:03:07 +1000

    Modification of variable names and function names.

--- member.java
+++ member.java
@@ -7,73 +7,67 @@ import java.util.Map;
 @SuppressWarnings("serial")
 public class member implements Serializable {
 
-	private String memberLastName;
-	private String memberFirstName;
-	private String memberEmail;
-	private int memberPhoneNumber;
-	private int memberId;
-	private double memberFines;
+	private String ln;
+	private String fn;
+	private String em;
+	private int pn;
+	private int ID;
+	private double fines;
 	
-	private Map<Integer, loan> memberLoans;
+	private Map<Integer, loan> lns;
 
 	
 	public member(String lastName, String firstName, String email, int phoneNo, int id) {
-		this.memberLastName = lastName;
-		this.memberFirstName = firstName;
-		this.memberEmail = email;
-		this.memberPhoneNumber = phoneNo;
-		this.memberId = id;
-		this.memberLoans = new HashMap<>();//not sure what this do could someone fix workitout so is can update it
+		this.ln = lastName;
+		this.fn = firstName;
+		this.em = email;
+		this.pn = phoneNo;
+		this.ID = id;
+		
+		this.lns = new HashMap<>();
 	}
 
 	
 	public String toString() {
-		StringBuilder memberStringBuilder = new StringBuilder();
-		memberStringBuilder.append("Member:  ");
-		memberStringBuilder.append(memberId);
-		memberStringBuilder.append("\n");
-		memberStringBuilder.append("  Name:  ");
-		memberStringBuilder.append(memberLastName);
-		memberStringBuilder.append(", ");
-		memberStringBuilder.append(memberFirstName);
-		memberStringBuilder.append("\n");
-		memberStringBuilder.append("  Email: ");
-		memberStringBuilder.append(memberEmail);
-		memberStringBuilder.append("\n");
-		memberStringBuilder.append("  Phone: ");
-		memberStringBuilder.append(memberPhoneNumber);
-		memberStringBuilder.append("\n");
-		memberStringBuilder.append(String.format("  Fines Owed :  $%.2f", memberFines));
-		memberStringBuilder.append("\n");
+		StringBuilder sb = new StringBuilder();
+		sb.append("Member:  ").append(ID).append("\n")
+		  .append("  Name:  ").append(ln).append(", ").append(fn).append("\n")
+		  .append("  Email: ").append(em).append("\n")
+		  .append("  Phone: ").append(pn)
+		  .append("\n")
+		  .append(String.format("  Fines Owed :  $%.2f", fines))
+		  .append("\n");
 		
-		for (loan loan : memberLoans.values()) {
-			memberStringBuilder.append(loan).append("\n");
+		for (loan loan : lns.values()) {
+			sb.append(loan).append("\n");
 		}		  
-		return memberStringBuilder.toString();
-	}
-
-	public int getId() {
-		return memberId;
-	}
-
-	public List<loan> getLoans() {
-		return new ArrayList<loan>(memberLoans.values());
+		return sb.toString();
 	}
 
 	
-	public int getNumberOfCurrentLoans() {
-		return memberLoans.size();
+	public int get_ID() {
+		return ID;
 	}
 
 	
-	public double getFinesOwed() {
-		return memberFines;
+	public List<loan> get_Loans() {
+		return new ArrayList<loan>(lns.values());
+	}
+
+	
+	public int currentLoanAmount() {
+		return lns.size();
+	}
+
+	
+	public double finesOwed() {
+		return fines;
 	}
 
 	
 	public void takeOutLoan(loan loan) {
-		if (!memberLoans.containsKey(loan.getLoanId())) {
-			memberLoans.put(loan.getLoanId(), loan);
+		if (!lns.containsKey(loan.ID())) {
+			lns.put(loan.ID(), loan);
 		}
 		else {
 			throw new RuntimeException("Duplicate loan added to member");
@@ -82,17 +76,17 @@ public class member implements Serializable {
 
 	
 	public String getLastName() {
-		return memberLastName;
+		return ln;
 	}
 
 	
 	public String getFirstName() {
-		return memberFirstName;
+		return fn;
 	}
 
 
 	public void addFine(double fine) {
-		memberFines += fine;
+		fines += fine;
 	}
 	
 	public double payFine(double amount) {
@@ -100,21 +94,24 @@ public class member implements Serializable {
 			throw new RuntimeException("Member.payFine: amount must be positive");
 		}
 		double change = 0;
-		if (amount > memberFines) {
-			change = amount - memberFines;
-			memberFines = 0;
+		if (amount > fines) {
+			change = amount - fines;
+			fines = 0;
 		}
 		else {
-			memberFines -= amount;
+			fines -= amount;
 		}
 		return change;
 	}
+
+
 	public void dischargeLoan(loan loan) {
-		if (memberLoans.containsKey(loan.getLoanId())) {
-			memberLoans.remove(loan.getLoanId());
+		if (lns.containsKey(loan.ID())) {
+			lns.remove(loan.ID());
 		}
 		else {
 			throw new RuntimeException("No such loan held by member");
 		}		
 	}
+
 }
